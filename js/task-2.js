817bd276b35d8c120f14d6de74e153a0c5b442f9
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-02-07 03:18:57 +0200

    Implement Storage class with methods to manage items and add initial tests

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,31 @@
+'use strict';
 
+console.log("<=== TASK-2. Склад =========================================================>");
+
+class Storage {
+  #items; // Приватна властивість
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter(item => item !== itemToRemove);
+  }
+}
+
+// Перевірка роботи
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
